b2641e099e496142e4c4312913ba7c2f1e635962
Mario Corchero <mcorcherojim@bloomberg.net>
2022-07-06 14:13:00 +0200

    Add chapter 9

---
+++ Chapter09/Exercise126/eratosthenes.py
@@ -0,0 +1,14 @@
+import timeit
+
+class PrimesBelow:
+    def __init__(self, bound):
+        self.candidate_numbers = list(range(2,bound))
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if len(self.candidate_numbers) == 0:
+            raise StopIteration
+        next_prime = self.candidate_numbers[0]
+        self.candidate_numbers = [x for x in self.candidate_numbers if x % next_prime != 0]
+        return next_prime
+print(timeit.timeit('list(PrimesBelow(1000))', setup='from __main__ import PrimesBelow', number=10000))
